316a3a4b8380d8048f9496e00bacb46100f1654e
6DDUU6 <51308585+6DDUU6@users.noreply.github.com>
2022-09-12 00:56:22 +0800

    Update train_ms.py

--- train_ms.py
+++ train_ms.py
@@ -106,8 +106,8 @@ def run(rank, n_gpus, hps):
   net_d = DDP(net_d, device_ids=[rank], find_unused_parameters=True)
 
   try:
-    _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G_*.pth"), net_g, optim_g)
-    _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D_*.pth"), net_d, optim_d)
+    _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "G.pth"), net_g, optim_g)
+    _, _, _, epoch_str = utils.load_checkpoint(utils.latest_checkpoint_path(hps.model_dir, "D.pth"), net_d, optim_d)
     global_step = (epoch_str - 1) * len(train_loader)
   except:
     epoch_str = 1
